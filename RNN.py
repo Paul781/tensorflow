2b7b40426ab2b364cf7f1f83e90ffeead75823b7
Michael <sundong781@gmail.com>
2017-03-22 15:19:30 +1100

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -115,7 +115,9 @@ with tf.Session() as sess:
     step = 0
     while step * batch_size < training_iters:
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs]) #batch_size means n_sample,n_input is the column number, n_step is the row number
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs]) 
+        #batch_size means n_sample,n_input is the column number, n_step is the row number
+        #batch_xs.shape is (batch_size,28*28)
         sess.run([train_op], feed_dict={
             x: batch_xs,
             y: batch_ys,
